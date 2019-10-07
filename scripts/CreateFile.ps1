3302cffda7d917e05f9f68e3fa05a8d092d6ad3f
Matías Creimerman <mcrei@email.com>
2019-10-07 20:07:53 -0300

    PowerShell Create File

---
+++ scripts/CreateFile.ps1
@@ -0,0 +1,24 @@
+﻿<#
+    .DESCRIPTION
+        Create File (check if exists)
+    .NOTES
+        Create by Matías Creimerman
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $True)]
+    [String] $FileName)
+
+if (-not (Test-Path $FileName -PathType Leaf)) {
+    
+    try {
+        New-Item -Path $FileName -ItemType File
+    }
+    catch {
+        Write-Error -Message "Unable to create file '$FileName'. Error: $_" -ErrorAction Stop;
+    }
+    "Successfully created! '$FileName'.";
+}
+else {
+    "File already exists!";
+}
