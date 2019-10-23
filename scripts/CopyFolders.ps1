77468d7aa8d8804b5acdbdbc278c37d65846e7ad
Matías Creimerman <mcrei@email.com>
2019-10-23 19:02:15 -0300

    Add files via upload

---
+++ scripts/CopyFolders.ps1
@@ -0,0 +1,35 @@
+﻿<#
+    .DESCRIPTION
+        Copy Folders (check if exists)
+    .NOTES
+        Create by Matías Creimerman
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $True)]
+    [String] $FromPath,
+    [Parameter(Mandatory = $True)]
+    [String] $ToPath,
+    [Parameter(Mandatory = $True)]
+    [String] $Filter,
+    [Parameter(Mandatory = $True)]
+    [String] $Recursive
+    )
+
+if ((Test-Path -LiteralPath $FromPath) -and (Test-Path -LiteralPath $ToPath)) {
+    try {
+        if ($Recursive){
+            Copy-Item -Filter $Filter -Path $FromPath -Recurse -Destination $ToPath;
+        }
+        else{
+            Copy-Item -Filter $Filter -Path $FromPath -Destination $ToPath;
+        }
+    }
+    catch {
+        Write-Error -Message "Unable to copy folder. Error: $_" -ErrorAction Stop;
+    }
+    "Successfully copied! '.";
+}
+else {
+    "Folder not exists!";
+}
