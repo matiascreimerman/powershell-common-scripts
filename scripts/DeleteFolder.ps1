d886603ba9ed63960ad55509a064ae3f662a983b
Matías Creimerman <mcrei@email.com>
2019-10-23 19:07:33 -0300

    Add files via upload

---
+++ scripts/DeleteFolder.ps1
@@ -0,0 +1,31 @@
+﻿<#
+    .DESCRIPTION
+        Delete Folder
+    .NOTES
+        Create by Matías Creimerman
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $True)]
+    [String] $Path,
+    [Parameter(Mandatory = $True)]
+    [String] $Recursive
+    )
+
+if (Test-Path -LiteralPath $Path) {
+    try {
+        if ($Recursive){
+            Remove-Item $Path -Recurse;
+        }
+        else{
+            Remove-Item $Path;
+        }
+    }
+    catch {
+        Write-Error -Message "Unable to copy folder. Error: $_" -ErrorAction Stop;
+    }
+    "Successfully deleted! '.";
+}
+else {
+    "Folder not exists!";
+}
